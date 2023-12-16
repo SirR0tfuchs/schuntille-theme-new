75d49f9edfabc4796fef282fe3122255e1ca97a0
rotfuchs <jannes.schuett@gmx.net>
2023-12-16 18:30:45 +0100

    added changes to correct slide to

--- static/js/own.js
+++ static/js/own.js
@@ -14,6 +14,12 @@ document.addEventListener("DOMContentLoaded", () => {
       prevEl: '.swiper-button-prev',
     },
 
-
     });
+
+  Array.from(document.getElementsByClassName("links")).forEach( (element, index) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+      swiper.slideTo(index, 500, false);
+    })
+  })
 })
