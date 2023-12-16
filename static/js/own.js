ab0b9f77a53c9cce225a86a47034c6f2c3de7023
rotfuchs <jannes.schuett@gmx.net>
2023-12-16 16:51:00 +0100

    fun side project completed

--- static/js/own.js
+++ static/js/own.js
@@ -15,12 +15,32 @@ gsap.registerPlugin(ScrollTrigger);
 
 // above is needed
 
-let tl = gsap.timeline({repeat: -1})
+document.addEventListener("DOMContentLoaded", function(event) {
 
-tl.from("#oeffnungszeiten", {
+  let display_elements = document.getElementsByClassName("displayElement");
+  console.log(display_elements);
+  Array.from(display_elements).forEach( (element) => {
+  gsap.timeline({
+  scrollTrigger: {
+    trigger: element,
+    pin: true,
+    start: "-30% center",
+    end: "500% 0",
+    scrub: 0,
+    markers: false,
+    }
+  })
+  .to(element, {
+    opacity: 1,
+    y: -250,
+    duration: 0.5
+  })
+  .to(element, {
+    duration: 0.5,
+  })
+  .to(element, {
     opacity: 0,
-})
-
-tl.to("#oeffnungszeiten", {
-    opacity: 0,
-})
+    duration: 0.3
+  }, 0.9);
+  });
+  })
